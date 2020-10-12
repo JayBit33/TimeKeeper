4abf94b16e98d2042f7f0f617d50c6b68bcf39fe
Storm Hamilton <hamilsu@mail.uc.edu>
2020-10-11 20:37:41 -0400

    Added a setWage method to make sure a negative value cannot be passed in.

--- src/main/java/com/timekeeper/app/dto/Employee.java
+++ src/main/java/com/timekeeper/app/dto/Employee.java
@@ -11,6 +11,19 @@ public @Data class Employee {
     private PayStructure payType;
     private double wage; // can be set to annual salary or hourly wage.
 
+
+    /**
+     * Sets the employee's wage after making sure the value isn't a negative number
+     * @param wage A double that reflects the employee's wage.
+     */
+    public void setWage(double wage){
+        if (wage<0.0){
+        wage = 0.0;
+        }
+        this.wage = wage;
+    }
+
+
     public String getFullName() {
         return this.firstName + " " + this.lastName;
     }
