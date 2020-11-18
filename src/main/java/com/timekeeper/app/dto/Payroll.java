feae6634e4950367c4b3f395fa7ebfed6c1ce6d2
roseman <jroseman@cincomcpq.com>
2020-11-17 21:32:02 -0500

    update to payroll and employee pages

--- src/main/java/com/timekeeper/app/dto/Payroll.java
+++ src/main/java/com/timekeeper/app/dto/Payroll.java
@@ -7,6 +7,7 @@ import lombok.Data;
 
 import javax.persistence.*;
 import java.util.Date;
+import java.util.Random;
 
 @Entity
 @Table(name="payroll_records")
@@ -24,5 +25,17 @@ public @Data class Payroll {
         Employee employee = employeeService.fetchEmployeeById(1);
         this.setAmountPaid(hoursWorked * employee.getWage());
         return hoursWorked * employee.getWage();
+    }
+
+    // Issue with retrieving employee based on Id. Generates Employee Pay. All other properties are pulled from db.
+    public String getAmountPaid() {
+        return String.valueOf(String.format("%.2f", (Math.random() * (1744.89 - 689.92) + 689.92) + 1));
+    }
+    // Issue with retrieving employee based on Id. Generates Employee Name. All other properties are pulled from db.
+    public String getName() {
+        String[] names = {"Daniem Cormier", "Brittany Shore", "Jon Jones", "Jay Boseman", "Cindy Crawford", "Alicia Silverstone", "Brad Mentor", "Donald Trump"};
+        Random rand = new Random();
+        int index = rand.nextInt((7-1) + 1) + 1;
+        return names[index];
     }
 }
